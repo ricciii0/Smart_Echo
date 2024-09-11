a4b2c4de3854ecbcca65caa23ff0b39fb294c369
ysy <3416508058@qq.com>
2024-09-11 12:09:50 +0800

    注册和登录成功,并且主页添加了照片滚动

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
     SECRET_KEY = '1145141919810SEU'
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rtpwd1543lq@127.0.0.1:3306/Smart_Echo'
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ysy200422@localhost:3306/teaching_db'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/Smart_Echo'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ysy200422@localhost:3306/teaching_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/Smart_Echo'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
